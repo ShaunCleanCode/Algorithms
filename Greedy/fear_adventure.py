81d76451dfdbdc2ec1fd335fdff6b53c6c390cf6
Shaun <shaun.on.us@gmail.com>
2024-04-12 20:22:14 +0900

    solve problem return index that makes palindrome

---
+++ Greedy/fear_adventure.py
@@ -0,0 +1,26 @@
+# 모험가의 수 N 명 
+n=int(input())
+fear_list=list(map(int(input().split()))).sort()
+# 공포도가 X 인 모험가는 반드시 X 명이상 구성 
+
+# Example ( N=5 , 2 3 1 2 2 )
+
+
+# 공포도를 먼저 선택 후 , 사람을 집합?
+# 공포도를
+
+result=0
+count=0
+for i in fear_list:
+    count+=1
+    if i <=count:
+        result+=1
+        count=0
+        
+
+        
+    
+    
+    
+    
+
